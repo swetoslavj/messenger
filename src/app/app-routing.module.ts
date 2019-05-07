4afa5783b34aa2b6fb990c1d300b9af3fdc54b17
swetoslavj <37697665+swetoslavj@users.noreply.github.com>
2019-05-07 09:58:13 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MessengerComponent } from './components/messenger/messenger.component';
+import { MessengerResolver } from './core/resolvers/messenger.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/messenger'
+  },
+  {
+    path: 'messenger',
+    component: MessengerComponent,
+    resolve: { messengerData: MessengerResolver }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
